23c320699b085525252e4a2f70aa968978186b80
EvgChu <chuevgenii@gmail.com>
2022-05-18 14:43:47 +0300

    initial commit

---
+++ builder/builder.py
@@ -0,0 +1,47 @@
+
+
+
+class HtmlElement:
+    indent_size = 2
+
+    def __init__(self, name='', text='') -> None:
+        self.text = text
+        self.name = name
+        self.elements = []
+
+    def __str(self, indent):
+        lines = []
+        i = ' ' * (indent * self.indent_size)
+        lines.append(f'{i}<{self.name}>')
+
+        if self.text:
+            i1 = ' ' * ((indent + 1) * self.indent_size)
+            lines.append(f'{i1}{self.text}')
+
+        for e in self.elements:
+            lines.append(e.__str(indent + 1))
+
+        lines.append(f'{i}</{self.name}>')
+        return '\n'.join(lines)
+
+    def __str__(self):
+        return self.__str(0)
+
+class HtmlBuilder:
+    def __init__(self, root_name):
+        self.root_name = root_name
+        self.__root = HtmlElement(root_name)
+
+    def add_child(self, child_name, child_text):
+        self.__root.elements.append(
+            HtmlElement(child_name, child_text)
+        )
+
+    def __str__(self) -> str:
+        return str(self.__root)
+
+if __name__ == "__main__":
+    builder = HtmlBuilder('ul')
+    builder.add_child('li','first')
+    builder.add_child('li','second')
+    print(builder)
---
+++ readme.md
@@ -0,0 +1,2 @@
+# The PythonDesignPatterns
+link on course Django https://stepik.org/course/103603/syllabus
